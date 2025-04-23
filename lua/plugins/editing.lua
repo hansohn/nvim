621fb18ba35d38761ff396cdfba45c2ace4145bb
Ryan Hansohn <hansohn@users.noreply.github.com>
2025-04-23 11:47:39 -0700

    add updates

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -35,14 +35,14 @@ return {
       end)
     end,
     opts = function()
-      local plugin = require("lazy.core.config").plugins["conform.nvim"]
-      if plugin.config ~= M.setup then
-        LazyVim.error({
-          "Don't set `plugin.config` for `conform.nvim`.\n",
-          "This will break **LazyVim** formatting.\n",
-          "Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
-        }, { title = "LazyVim" })
-      end
+      -- local plugin = require("lazy.core.config").plugins["conform.nvim"]
+      -- if plugin.config ~= M.setup then
+      --   LazyVim.error({
+      --     "Don't set `plugin.config` for `conform.nvim`.\n",
+      --     "This will break **LazyVim** formatting.\n",
+      --     "Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
+      --   }, { title = "LazyVim" })
+      -- end
       ---@type conform.setupOpts
       local opts = {
         default_format_opts = {
@@ -231,6 +231,8 @@ return {
   -- emoji blink.cmp integration
   {
     "saghen/blink.cmp",
+    build = "cargo build --release",
+    version = "*",
     dependencies = { "allaman/emoji.nvim", "saghen/blink.compat" },
     opts = {
       sources = {
